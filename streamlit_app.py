e7f281795c019c99e67f679644c95270a3cdf57e
PaulKar9 <pkardash@gmail.com>
2023-10-25 11:23:28 -0600

    Update streamlit_app.py
    Adding Breakfast header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner - TAKEOVER')
+streamlit.header('Breakfast Manu')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
