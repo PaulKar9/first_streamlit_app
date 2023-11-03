4eb23f333b7c0ff2af34ebad8ec0bc4887f160f3
PaulKar9 <pkardash@gmail.com>
2023-11-03 13:55:22 -0600

    Added icons and avocado toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner - TAKEOVER')
 streamlit.header('Breakfast Manu')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Eggs')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Eggs')
+streamlit.text('🥑🍞 Avocado Toast')
 
