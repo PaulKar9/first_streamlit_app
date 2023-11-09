599e0724ab332edc770b9209a6f83f438008bc83
PaulKar9 <pkardash@gmail.com>
2023-11-09 10:10:51 -0700

    Thank you text for adding a fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ add_my_fruit = streamlit.multiselect("What fruit would you like to add:", ['Avoc
 #fruit_to_add = my_data_rows.loc[add_my_fruit]
 
 # Display the fruit to add.
-#streamlit.write("Thanks for adding ", fruit_to_add)
+streamlit.write("Thanks for adding ", add_my_fruit)
